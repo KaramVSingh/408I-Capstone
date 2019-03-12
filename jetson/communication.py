d4ad94dfa7dc345d4f5e87ed889ceee43edd8b17
Karam Singh <karamsingh175@gmail.com>
2019-03-12 18:23:55 +0100

    removed comments

--- jetson/communication.py
+++ jetson/communication.py
@@ -18,22 +18,3 @@ def read():
 def close():
 	ser.close()
 
-'''
-class Communication:
-	def __init__(self):
-		self.ser = serial.Serial('/dev/ttyACM0')
-		time.sleep(SLEEP_TIME)
-
-	def send(self, message):
-		print('sending ' + str(message))
-		self.ser.write(message)
-	
-	def read(self):
-		if self.ser.in_waiting:
-			return self.ser.readline()
-		else:
-			return None
-
-	def close(self):
-		self.ser.close()
-'''
